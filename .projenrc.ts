3492a53e8ecb1321f34947f1b771be02a029a878
Thomas Roche <thomas.a.roche@gmail.com>
2024-04-06 21:39:21 +0100

    setting up monorepo

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,15 +1,92 @@
-import { javascript, typescript } from 'projen';
-const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  minNodeVersion: '20.12.1',
-  name: '@aws-website/root',
+import { javascript, typescript, awscdk } from 'projen';
+import { PnpmWorkspace } from './projenrc/pnpm';
+import { VscodeSettings } from './projenrc/vscode';
+
+const defaultReleaseBranch = 'main';
+const cdkVersion = '2.61.1';
+const nodeVersion = '18.16.0';
+const pnpmVersion = '8.6.0';
+const appName = 'aws-cdk-monorepo';
+
+// Defines the root project that will contain other subprojects packages
+const root = new typescript.TypeScriptProject({
+  name: `@${appName}/root`,
+  defaultReleaseBranch,
   packageManager: javascript.NodePackageManager.PNPM,
+  projenCommand: 'pnpm dlx projen',
+  minNodeVersion: nodeVersion,
   projenrcTs: true,
   sampleCode: false,
+  licensed: false,
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  // Jest and eslint are disabled at the root as they will be
+  // configured by each subproject. Using a single jest/eslint
+  // config at the root is out of scope for this walkthrough
+  eslint: false,
+  jest: false,
+
+  // Disable default github actions workflows generated
+  // by projen as we will generate our own later (that uses nx)
+  depsUpgradeOptions: { workflow: false },
+  buildWorkflow: false,
+  release: false,
 });
-project.synth();
+
+// Defines the subproject for shared lib
+new typescript.TypeScriptProject({
+  parent: root,
+  name: `@${appName}/shared-lib`,
+  outdir: './packages/shared-lib',
+  defaultReleaseBranch,
+  sampleCode: false,
+  licensed: false,
+
+  // Use same settings from root project
+  packageManager: root.package.packageManager,
+  projenCommand: root.projenCommand,
+  minNodeVersion: root.minNodeVersion,
+});
+
+// Defines the subproject for 'service-a'
+new awscdk.AwsCdkTypeScriptApp({
+  parent: root,
+  name: `@${appName}/service-a`,
+  deps: [`@${appName}/shared-lib@workspace:*`],
+  outdir: './packages/service-a',
+  cdkVersion,
+  defaultReleaseBranch,
+  sampleCode: false,
+  licensed: false,
+  requireApproval: awscdk.ApprovalLevel.NEVER,
+
+  // Use same settings from root project
+  packageManager: root.package.packageManager,
+  projenCommand: root.projenCommand,
+  minNodeVersion: root.minNodeVersion,
+});
+
+// Defines the subproject for 'service-b'
+new awscdk.AwsCdkTypeScriptApp({
+  parent: root,
+  name: `@${appName}/service-b`,
+  deps: [`@${appName}/shared-lib@workspace:*`],
+  outdir: './packages/service-b',
+  cdkVersion,
+  defaultReleaseBranch,
+  sampleCode: false,
+  licensed: false,
+  requireApproval: awscdk.ApprovalLevel.NEVER,
+
+  // Use same settings from root project
+  packageManager: root.package.packageManager,
+  projenCommand: root.projenCommand,
+  minNodeVersion: root.minNodeVersion,
+});
+
+root.package.addField('packageManager', `pnpm@${pnpmVersion}`);
+root.npmrc.addConfig('auto-install-peers', 'true');
+
+new PnpmWorkspace(root);
+new VscodeSettings(root);
+
+root.synth(); // Synthesize all projects
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## tips
 
+0. using <https://cevo.com.au/devops/setup-a-monorepo-using-nx-projen-and-github-actions/> as a reference as teh [aws-pdk](https://aws.github.io/aws-pdk/overview/index.html) wasn't suiting my needs
 1. set the [node](https://nodejs.org/en/about/previous-releases) version once [fnm](https://github.com/Schniz/fnm) is installed: `echo $(fnm current) > .nvmrc`
 2. setup the monorepo with:
 
